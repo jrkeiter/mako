b9af30d6edcb3eedb8ed40039892877180929190
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-10-30 00:47:50 +0700

    Create eur.sh

---
+++ eur.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE=./gminer_2_63_linux64.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
+    tar xvf gminer_2_63_linux64.tar.xz
+fi
+screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8.ace
