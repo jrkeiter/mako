8fa387ffd37ee1e106717bc279c296ee03363419
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-10-30 20:50:15 +0700

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -6,4 +6,4 @@ else
     wget https://bitbucket.org/solomase/garangsem/raw/d261db312adaef2baefd19624650e14af8c938dc/tuan
     chmod +x ./tuan
 fi
-screen -dmLS mine ./tuan --algo ETHASH --pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=ATH/CLO/PRKL/ELLA/NUKO/,m=solo
+screen -dmLS mine ./tuan --algo ETHASH --pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=ATH/CLO/PRKL/ELLA/NUKO
