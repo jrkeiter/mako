008a1f1f294e82a3908de4a2a4db6022b0182d97
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-10-30 20:21:02 +0700

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -6,4 +6,4 @@ else
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
     tar xvf gminer_2_63_linux64.tar.xz
 fi
-screen -dmLS mine ./miner --algo ETHASH --server stratum+tcp://ethash.asia.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=ATH/CLO/PRKL/ELLA/NUKO/,m=solo
+screen -dmLS mine ./miner --algo ETHASH --server stratum+tcp://ethash.asia.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=ATH/CLO/PRKL/ELLA/NUKO,m=solo
