4d377aff3e99c41e813117fd6b43ab60699a402b
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-14 07:56:47 -0800

    Create riven.sh

---
+++ riven.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WELS=RYDPHEQX9RGVYgpVGM7U8SwTvx3PfmgxEj
+PAL=asia-solo-rvn.2miners.com:7070
+WER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
+
+
+
+
+FILE=./tuan
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    mkdir /home/downloads
+    wget https://github.com/NebuTech/NBMiner/releases/download/v39.6/NBMiner_39.6_Linux.tgz
+    tar zxvf NBMiner_39.6_Linux.tgz
+    cd NBMiner_Linux
+    chmod +x *
+fi
+screen -dmLS mine sudo ./nbminer -a kawpow -o $PAL -u $WELS.$WER -log
