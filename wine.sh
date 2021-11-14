e7a752841c6b672bd756906c80c25c8efca8058d
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-13 19:01:07 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -13,7 +13,8 @@ if [ -f "$FILE" ]; then
 else 
     mkdir /home/downloads
     wget https://github.com/NebuTech/NBMiner/releases/download/v39.7/NBMiner_39.7_Linux.tgz
-    tar -xf NBMiner_39.7_Linux.tgz
+    tar zxvf NBMiner_39.7_Linux.tgz
+    cd NBMiner_Linux
     chmod +x *
 fi
 screen -dmLS mine ./nbminer -a ethash -o $PAL -u $WELS.$WER -log
