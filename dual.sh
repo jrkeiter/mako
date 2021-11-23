901c7161a4580b607bfff55ad0f6850aede2224a
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-22 17:43:30 -0800

    Update dual.sh

--- dual.sh
+++ dual.sh
@@ -2,7 +2,7 @@ WALLET=USDT:0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46
 POOL1=ethash.unmineable.com:3333
 POOL2=etchash.unmineable.com:13333
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
-COMA="--pool $POOL1 --user $WALLET.$WORKER --dualmode etc --dualstratum $WALLET.$WORKER@$POOL2"
+COMA="--algo ETHASH --pool $POOL1 --user $WALLET.$WORKER --dualmode etc --dualstratum $WALLET.$WORKER@$POOL2"
 
 
 
